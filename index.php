717f2cb5e55996dd72587df18b0043df1c1ab9b4
afiqahhassan <61734893+afiqahsssssssss@users.noreply.github.com>
2021-04-06 21:58:33 +0800

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-  <form method="POST" action="balance.php">
-  </form>
-  <form method="POST" action="reload.php">
-    <input type="submit" name="+Reload" value="+Reload">
-  </form>
-</body>
-</html>
