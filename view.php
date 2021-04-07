5619a4a88e9769b230c7eddab3f35dbd21f5208c
afiqahhassan <61734893+afiqahsssssssss@users.noreply.github.com>
2021-04-07 09:06:40 +0800

    Update view.php

--- view.php
+++ view.php
@@ -1,18 +1,17 @@
+
 <?php
-
-
-
+$balance= 500;
 if (isset($_POST['submit'])) {
   // $balance = $_POST['bal'];
   $typoftrans = $_POST['amount'];
   $topup = $_POST['rm'];
   $date = $_POST['datetrans'];
-  echo "<b>If you use credit card you will get RM0.50 cashback<b><br>";
+
   if($typoftrans <= 1)
-  $balanceafter = $topup + 500 + 0.5 ;
-  
+  $balance= $topup + $balance + 0.5 ;
+
   else {
-  $balanceafter  = $topup + 500;
+  $balance  = $topup + $balance;
   }
   // if (isset($_POST['amount'])) {
 
@@ -25,57 +24,46 @@ if (isset($_POST['submit'])) {
 
 $users = array (
 
-  
   array("Topup", $topup),
   array("Date",$date),
-  array("Remaining balance",$balanceafter),
-  
-  
-  
+  array("Remaining balance",$balance),
+
 );
-
-
-
 
 echo $users[0][0].": RM" .$users[0][1]."<br>";
 echo $users[1][0].": " .$users[1][1]."<br>";
 echo $users[2][0].": RM" .$users[2][1]."<br>";
-
 // echo $users[3][0].": " .$users[3][1]."<br>";
 }
 
 elseif (isset($_POST['submit2'])) {
   // $balance2 = $_POST['bal2'];
   $payment = $_POST['pay'];
-  // $date2 = $_POST['datetrans2'];
+  $date2 = $_POST['datetrans2'];
   $time = $_POST['timetrans'];
   if ($time >= '12:00' && $time <= '14:00' )
   {
     // $dis = 0;
-    $discount = 500 - ($payment * 0.90) ;
-    echo "<b>10% discount on Peak Hours only at 12pm until 2pm <b><br>";
+    $balance = $balance - ($payment * 0.90) ;
   }
   else {
 
-    $discount = 500 - $payment;
- 
+    $balance = $balance - $payment;
   }
 
   $data = array (
     // array("Balance","500"),
     array("Payment", $payment),
-    // array("Date",$date2),
+    array("Date",$date2),
     array("Time",$time),
-    array("Remaining balance",$discount),
-    
+    array("Remaining balance",$balance),
+
   );
-  
   echo $data[0][0].": RM" .$data[0][1]."<br>";
   echo $data[1][0].": " .$data[1][1]."<br>";
-  echo $data[2][0].": RM" .$data[2][1]."<br>";
-  // echo $data[3][0].": RM" .$data[3][1]."<br>";
+  echo $data[2][0].": " .$data[2][1]."<br>";
+  echo $data[3][0].": RM" .$data[3][1]."<br>";
   // echo $data[4][0].". " .$data[4][1]."<br>";
 
 }
-
  ?>
