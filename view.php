3bb1845dd03651c4741e5e18fac0f542d24d6551
afiqahhassan <61734893+afiqahsssssssss@users.noreply.github.com>
2021-04-07 08:33:23 +0800

    Update view.php

--- view.php
+++ view.php
@@ -3,14 +3,12 @@
 
 
 if (isset($_POST['submit'])) {
-  // $balance = $_POST['bal'];
   $typoftrans = $_POST['amount'];
   $topup = $_POST['rm'];
   $date = 
 <?php
 $balance= 500;
 if (isset($_POST['submit'])) {
-  // $balance = $_POST['bal'];
   $typoftrans = $_POST['amount'];
   $topup = $_POST['rm'];
   $date = $_POST['datetrans'];
@@ -21,15 +19,7 @@ if (isset($_POST['submit'])) {
   else {
   $balance  = $topup + $balance;
   }
-  // if (isset($_POST['amount'])) {
-
-
-// if (isset($_POST['rm'])) {
-//   $topup = $_POST['rm'];
-//
-// if (isset($_POST['datetrans'])){
-//  $date = $_POST['datetrans'];
-
+ 
 $users = array (
 
   array("Topup", $topup),
@@ -41,17 +31,16 @@ $users = array (
 echo $users[0][0].": RM" .$users[0][1]."<br>";
 echo $users[1][0].": " .$users[1][1]."<br>";
 echo $users[2][0].": RM" .$users[2][1]."<br>";
-// echo $users[3][0].": " .$users[3][1]."<br>";
-}
+
 
 elseif (isset($_POST['submit2'])) {
-  // $balance2 = $_POST['bal2'];
+  
   $payment = $_POST['pay'];
   $date2 = $_POST['datetrans2'];
   $time = $_POST['timetrans'];
   if ($time >= '12:00' && $time <= '14:00' )
   {
-    // $dis = 0;
+    
     $balance = $balance - ($payment * 0.90) ;
   }
   else {
@@ -60,7 +49,7 @@ elseif (isset($_POST['submit2'])) {
   }
 
   $data = array (
-    // array("Balance","500"),
+    
     array("Payment", $payment),
     array("Date",$date2),
     array("Time",$time),
@@ -71,7 +60,7 @@ elseif (isset($_POST['submit2'])) {
   echo $data[1][0].": " .$data[1][1]."<br>";
   echo $data[2][0].": " .$data[2][1]."<br>";
   echo $data[3][0].": RM" .$data[3][1]."<br>";
-  // echo $data[4][0].". " .$data[4][1]."<br>";
+  
 
 }
  ?>
@@ -83,15 +72,7 @@ elseif (isset($_POST['submit2'])) {
   else {
   $balanceafter  = $topup + 500;
   }
-  // if (isset($_POST['amount'])) {
-
-
-// if (isset($_POST['rm'])) {
-//   $topup = $_POST['rm'];
-//
-// if (isset($_POST['datetrans'])){
-//  $date = $_POST['datetrans'];
-
+ 
 $users = array (
 
   array("Topup", $topup),
@@ -103,11 +84,10 @@ $users = array (
 echo $users[0][0].": RM" .$users[0][1]."<br>";
 echo $users[1][0].": " .$users[1][1]."<br>";
 echo $users[2][0].": RM" .$users[2][1]."<br>";
-// echo $users[3][0].": " .$users[3][1]."<br>";
-}
+
 
 elseif (isset($_POST['submit2'])) {
-  // $balance2 = $_POST['bal2'];
+  
   $payment = $_POST['pay'];
   // $date2 = $_POST['datetrans2'];
   $time = $_POST['timetrans'];
